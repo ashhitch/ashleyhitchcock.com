f56e23133e64ca39d73043dfb33b3cddc59329d2
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2020-04-19 19:41:29 +0100

    uses

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'gatsby';
 import { Heading } from 'grommet';
 import Layout from '../components/layout';
@@ -8,28 +8,57 @@ import IntroWrap from '../components/intro';
 import Techstack from '../components/techstack';
 import HtmlContent from '../components/htmlContent';
 
+import useTypewriter from 'react-typewriter-hook';
 
-const IndexPage = () => (
-    <Layout>
-        <SEO title="Home" />
-        <IntroWrap
-            pad={{
-                'vertical': 'large',
-                'horizontal': 'large',
-            }}
-            margin={{
-                'vertical': 'large',
-            }}
-        >
-            <Heading level="1">Hey people 👋</Heading>
-            <HtmlContent>
-                <p>I'm Ash Hitchcock. Slinger of Divs, Slayer of JavaScript, hater of !important, survivor of IE6; Front-end Development Director at Fresh Egg. Focused on continuing to grow in the field of web development backed up by over 15 years of creating for the web. Lover of all things JavaScript including React, TypeScript, Node.</p>
-                <p><Link to="/about">Read more</Link></p>
-            </HtmlContent>
 
-        </IntroWrap>
-        <Techstack />
-    </Layout>
-);
+const MagicOcean = [
+    'Slinger of Divs',
+    'Slayer of JavaScript',
+    'Hater of !important',
+    'survivor of IE6',
+];
+let index = 0;
+
+const IndexPage = () => {
+
+    const [magicName, setMagicName] = useState('Hey i\'m Ash Hitchcock 👋');
+    const intervalRef = useRef({});
+    const h1 = useTypewriter(magicName);
+    useEffect(
+        () => {
+            intervalRef.current = setInterval(() => {
+                index = index > 2 ? 0 : ++index;
+                setMagicName(MagicOcean[index]);
+            }, 5000);
+            return function clear() {
+                clearInterval(intervalRef.current);
+            };
+        },
+        [magicName]
+    );
+
+    return (
+        <Layout>
+            <SEO title="Home" />
+            <IntroWrap
+                pad={{
+                    'vertical': 'large',
+                    'horizontal': 'large',
+                }}
+                margin={{
+                    'vertical': 'large',
+                }}
+            >
+                <Heading level="1">{h1}</Heading>
+                <HtmlContent>
+                    <p>Ash Hitchcock, Front-end Development Director at Fresh Egg. Focused on continuing to grow in the field of web development backed up by over 15 years of creating for the web. Lover of all things JavaScript including React, TypeScript, Node.</p>
+                    <p><Link to="/about">Read more</Link></p>
+                </HtmlContent>
+
+            </IntroWrap>
+            <Techstack />
+        </Layout>
+    );
+};
 
 export default IndexPage;
