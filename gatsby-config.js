efe4785cec6a3d40b8fb1f8fc9c6330aa6650dad
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2020-04-28 11:15:52 +0100

    covert to ts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,74 @@
 module.exports = {
-    siteMetadata: {
-        title: 'Ashley Hitchcock',
-        description: 'Slinger of <Divs />, Slayer of JavaScript, hater of !important, survivor&nbsp;of IE6; Front-end developer',
-        author: '@ash_hitchcock',
+  siteMetadata: {
+    title: 'Ashley Hitchcock',
+    description:
+      'Slinger of <Divs />, Slayer of JavaScript, hater of !important, survivor&nbsp;of IE6; Front-end developer',
+    author: '@ash_hitchcock',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
     },
-    plugins: [
-        'gatsby-plugin-react-helmet',
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: `${__dirname}/src/images`,
-            },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        displayName: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /icons/,
         },
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp',
-        {
-            resolve: 'gatsby-plugin-manifest',
-            options: {
-                name: 'gatsby-starter-default',
-                short_name: 'starter',
-                start_url: '/',
-                background_color: '#663399',
-                theme_color: '#663399',
-                display: 'minimal-ui',
-                icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-styled-components',
-            options: {
-                displayName: true,
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-react-svg',
-            options: {
-                rule: {
-                    include: /icons/ 
-                }
-            }
-        },
-        {
-            resolve: 'gatsby-plugin-google-fonts',
-            options: {
-                fonts: [
-    
-                    'Share Tech Mono:300,400,700' 
-                ],
-                display: 'swap'
-            }
-        },
-        'gatsby-plugin-remove-trailing-slashes',
-        {
-            resolve: 'gatsby-plugin-prettier-build',
-            options: {
-                types: ['html'],
-                concurrency: 20,
-                verbose: true,
-            },
-        },
-        {
-            resolve: 'gatsby-source-instagram',
-            options: {
-                username: 'frontendsnips',
-            },
-        },
-        'gatsby-plugin-netlify',
-        'gatsby-plugin-mdx',
-        'gatsby-plugin-use-dark-mode',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['Share Tech Mono:300,400,700'],
+        display: 'swap',
+      },
+    },
+    'gatsby-plugin-remove-trailing-slashes',
+    {
+      resolve: 'gatsby-plugin-prettier-build',
+      options: {
+        types: ['html'],
+        concurrency: 20,
+        verbose: true,
+      },
+    },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'frontendsnips',
+      },
+    },
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-use-dark-mode', // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    ],
+    'gatsby-plugin-typescript',
+  ],
 };
--- gatsby/createSnips.js
+++ gatsby/createSnips.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const slugify = require('slugify');
 
-
 module.exports = async ({ actions, graphql }) => {
-    const PER_PAGE = 8;
-    const GET_SNIPS = `
+  const PER_PAGE = 8;
+  const GET_SNIPS = `
   query GET_SNIPS($limit:Int $skip:Int){
       allInstaNode(
         limit: $limit 
@@ -26,62 +25,60 @@ module.exports = async ({ actions, graphql }) => {
       }
   }
   `;
-    const { createPage } = actions;
-    const allPosts = [];
-    const snipPages = [];
-    let pageNumber = 0;
-    const fetchPosts = async variables =>
-        graphql(GET_SNIPS, variables).then(({ data }) => {
-            const {
-                allInstaNode: {
-                    edges,
-                    pageInfo: { hasNextPage, hasPreviousPage, currentPage },
-                },
-            } = data;
+  const { createPage } = actions;
+  const allPosts = [];
+  const snipPages = [];
+  let pageNumber = 0;
+  const fetchPosts = async variables =>
+    graphql(GET_SNIPS, variables).then(({ data }) => {
+      const {
+        allInstaNode: {
+          edges,
+          pageInfo: { hasNextPage, hasPreviousPage, currentPage },
+        },
+      } = data;
 
+      const nodeIds = edges.map(edge => console.log(edge) || edge.node.id);
+      const snipTemplate = path.resolve('./src/templates/snips.tsx');
+      const snipPagePath = !variables.skip ? '/snips' : `/snips/${pageNumber}`;
 
-            const nodeIds = edges.map( (edge) => console.log(edge) || edge.node.id);
-            const snipTemplate = path.resolve('./src/templates/snips.jsx');
-            const snipPagePath = !variables.skip ? '/snips' : `/snips/${pageNumber}`;
-            console.log(snipPagePath);
-
-            snipPages[pageNumber] = {
-                path: snipPagePath,
-                component: snipTemplate,
-                context: {
-                    ids: nodeIds,
-                    pageNumber,
-                    hasNextPage,
-                    hasPreviousPage,
-                    currentPage,
-                },
-                ids: nodeIds,
-            };
-            edges.map(post => {
-                allPosts.push(post);
-            });
-            if (hasNextPage) {
-                pageNumber++;
-                return fetchPosts({ limit: PER_PAGE, skip: pageNumber * PER_PAGE });
-            }
-            return allPosts;
-        });
-
-    await fetchPosts({ limit: PER_PAGE, skip: 0 }).then(allPosts => {
-        // const postTemplate = path.resolve('./src/templates/snip.tsx');
-
-        snipPages.map(snipPage => {
-            console.log(`createSnipPage ${snipPage.context.pageNumber}`);
-            createPage(snipPage);
-        });
-
-        // allPosts.map(post => {
-        //     console.log(`create post: ${post.uri}`);
-        //     createPage({
-        //         path: `/snip/${post.uri}/`,
-        //         component: postTemplate,
-        //         context: post,
-        //     });
-        // });
+      snipPages[pageNumber] = {
+        path: snipPagePath,
+        component: snipTemplate,
+        context: {
+          ids: nodeIds,
+          pageNumber,
+          hasNextPage,
+          hasPreviousPage,
+          currentPage,
+        },
+        ids: nodeIds,
+      };
+      edges.map(post => {
+        allPosts.push(post);
+      });
+      if (hasNextPage) {
+        pageNumber++;
+        return fetchPosts({ limit: PER_PAGE, skip: pageNumber * PER_PAGE });
+      }
+      return allPosts;
     });
+
+  await fetchPosts({ limit: PER_PAGE, skip: 0 }).then(allPosts => {
+    // const postTemplate = path.resolve('./src/templates/snip.tsx');
+
+    snipPages.map(snipPage => {
+      console.log(`createSnipPage ${snipPage.context.pageNumber}`);
+      createPage(snipPage);
+    });
+
+    // allPosts.map(post => {
+    //     console.log(`create post: ${post.uri}`);
+    //     createPage({
+    //         path: `/snip/${post.uri}/`,
+    //         component: postTemplate,
+    //         context: post,
+    //     });
+    // });
+  });
 };
